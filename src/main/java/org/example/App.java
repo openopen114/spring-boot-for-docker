6ba37aa6ae58feb66c0a157f45ec3224849c4151
open open <openopen@opens-MacBook-Air.local>
2021-08-06 21:46:28 +0800

    use tst 002 db

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,19 +1,51 @@
 package org.example;
 
+import cloud.sql.Druid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @SpringBootApplication
 @RestController
 public class App {
-    public static void main(String[] args) {
+    private static final Logger logger
+            = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) throws IOException {
+
+
         SpringApplication.run(App.class, args);
+
+        //顯示 API 環境
+        getApiEnv();
     }
 
     @RequestMapping(value = "/")
     String hello() {
         return "Hello World!";
+    }
+
+
+
+
+    public static  String getApiEnv() throws IOException {
+        String fileName = "/DB.properties";
+        InputStream is = Druid.class.getResourceAsStream(fileName);
+        Properties p = new Properties();
+        p.load(is);
+
+        String APIENV = p.getProperty("APIENV");
+
+
+        logger.info("===================================");
+        logger.info("==========> APIENV: " + APIENV  +" <==========");
+        logger.info("===================================");
+        return APIENV;
     }
 }
